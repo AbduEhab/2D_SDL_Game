12af5d13ac028bb689682389c1fb8915320d9875
AbduEhab <abdu.ehab0@gmail.com>
2021-12-09 19:46:12 +0100

    FIXED THE GOD DAMN CMAKELISTS

--- src/Game.h
+++ src/Game.h
@@ -5,9 +5,9 @@
     if (DEBUG_ONLY) \
         x;
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
 #include "AssetManager.h"
 #include "Component.h"
