0d4e567923d1a569e98a123531cd669bd11ece0e
AbduEhab <abdu.ehab0@gmail.com>
2022-01-30 19:40:24 +0100

    testing 2

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,11 +18,10 @@ int main(int argc, char *arg[])
         float waitTime = FRAME_TIME_TARGET - (ticks_since_last_frame.count() * 1000); // calculate time to wait between frames
 
         if (waitTime > 0 && waitTime <= FRAME_TIME_TARGET)
-            SDL_Delay(waitTime); // hold exec untill the specified ms has passed
-        // std::this_thread::sleep_for(std::chrono::milliseconds((int)waitTime));
+            // SDL_Delay(waitTime); // hold exec untill the specified ms has passed
+            std::this_thread::sleep_for(std::chrono::milliseconds((int)waitTime));
 
         float delta_time = ticks_since_last_frame.count() * 1000; // diffrence between the time since the last frame and the current frame in seconds
-        DebugPrint(delta_time);
 
         game.ticks_last_frame = Clock::now(); // update the lastFrameTime time to be used in the next pass
 
